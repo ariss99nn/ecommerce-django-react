84bacec7a9817b99f4beddffaec5f7ff7994559c
ariss99nn <rodrigueznilo99@gmail.com>
2025-03-23 17:10:29 -0500

    Guardar configuración de base de datos

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,1 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, permission_required
-
-@login_required
-def productos(request):
-    return render(request, 'productos.html')
-
-@permission_required('store.change_producto', raise_exception=True)
-def editar_producto(request):
-    return render(request, 'editar_producto.html')
